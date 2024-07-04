086fe34daf4bf12d13d545320bb126cd3bce550f
Souvik Kundu <kivtech88@gmail.com>
2024-07-04 23:24:43 +0530

    commit

--- OddOrEven.java
+++ OddOrEven.java
@@ -1,20 +1,20 @@
 /*
-Write a program to check if the number is positive or negative.
+Write a program to check if the number is odd or even.
 */
 
 import java.util.Scanner;
 
-public class PositiveOrNegative {
+public class OddOrEven {
     public static void main (String args[]) {
         Scanner scanner = new Scanner (System.in);
 
         System.out.println("Enter a valid number: ");
         int num = scanner.nextInt();
         
-        if (num > 0) {
-            System.out.println(num + " is a positive integer");
+        if ((num % 2) == 0) {
+            System.out.println(num + " is an even number");
         } else {
-            System.out.println(num + " is a negative integer");
+            System.out.println(num + " is an odd number");
         }
         scanner.close();
     }
