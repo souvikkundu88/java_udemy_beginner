24e16ea4856aaf69475750a895d2251edc17cfb6
Souvik Kundu <kivtech88@gmail.com>
2024-07-04 19:27:44 +0530

    Update DataTypesAndOperators.java

--- DataTypesAndOperators.java
+++ DataTypesAndOperators.java
@@ -14,6 +14,18 @@ import java.util.Scanner;
 
 public class DataTypesAndOperators {
     public static void main (String args[]) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        // integer operations
+        System.out.println("Enter two integers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        System.out.println("num1 - num2 = " + (num1 - num2));
+        System.out.println("num1 / num2 = " + (num1 / num2));
+        System.out.println("num1 * num2 = " + (num1 * num2));
+        System.out.println("num1++ = " + (num1++));
+        System.out.println("num1-- = " + (num2--));
     }
 }
