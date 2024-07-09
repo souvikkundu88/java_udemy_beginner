29665dd78a4c19a1ebbb3ce40756277128695c8f
Souvik Kundu <kivtech88@gmail.com>
2024-07-09 16:51:53 +0530

    commit

--- PostvNegtvZero.java
+++ PostvNegtvZero.java
@@ -4,17 +4,19 @@ Write a program to check if the number is positive or negative.
 
 import java.util.Scanner;
 
-public class PositiveOrNegative {
+public class PostvNegtvZero {
     public static void main (String args[]) {
         Scanner scanner = new Scanner (System.in);
 
         System.out.println("Enter a valid number: ");
         int num = scanner.nextInt();
         
-        if (num > 0) {
-            System.out.println(num + " is a positive integer");
+        if (num == 0) {
+            System.out.println(num + " is a zero");
+        } else if (num > 0) {
+            System.out.println(num + " is a positive number");
         } else {
-            System.out.println(num + " is a negative integer");
+            System.out.println(num + " is a negative number");
         }
         scanner.close();
     }
